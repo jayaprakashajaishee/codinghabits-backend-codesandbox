2fb40c1626fe1a0e38366e5a3e3a7f98383474a2
jayaprakashajaishee <82564727+jayaprakashajaishee@users.noreply.github.com>
2022-04-18 10:02:07 +0530

    index updated for receiving command

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const express = require("express");
+import express from "express";
+import { exec } from "child_process";
+
+const PORT = 4200;
+
 const app = express();
-const port = 3000;
+
+app.use(express.json());
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.send("Hello World!");
+  res.send("Server is ready");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.post("/run", (req, res) => {
+  const { command } = req.body;
+
+  console.log(`[INFO] Running "${command}"`);
+
+  const child = exec(command);
+
+  child.stdout.setEncoding("utf8");
+  child.stderr.setEncoding("utf8");
+
+  let stdout = "";
+  let stderr = "";
+
+  child.stdout.on("data", (data) => {
+    stdout += data;
+  });
+
+  child.stderr.on("data", (data) => {
+    stderr += data;
+  });
+
+  child.on("exit", () => {
+    res.send({ stdout, stderr });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: http://localhost:${PORT}`);
 });
