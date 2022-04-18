945feea053c319d8746c5e0695ee5e8ed159d13c
jayaprakashajaishee <82564727+jayaprakashajaishee@users.noreply.github.com>
2022-04-18 10:04:10 +0530

    Sum.js addes

---
+++ Sum.js
@@ -0,0 +1,15 @@
+//Write a function to add two numbers
+//The function must accept two arguments as numbers
+//The function must return a number
+
+//Edit code here:
+function Sum(a, b) {
+  return a + b;
+}
+
+console.log(Sum(1, 3));
+console.log(Sum(1, 5));
+//console.log(sum(3));
+//console.log(Sum(a,b))
+
+module.exports = Sum;
